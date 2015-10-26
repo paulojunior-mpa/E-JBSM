087f1486b2e82e91a821d75eed5b8391cbba745e
willianrlm2 <willianw94@hotmail.com>
2015-10-26 14:51:23 -0200

    ert

--- webservice/plants.php
+++ webservice/plants.php
@@ -32,13 +32,14 @@ if (isset($_GET['imei'])) {
         $sql = "select id_imei from ejbsm_imei WHERE imei = $user_imei";
         $result = $link->query($sql);
         $id_imei = mysqli_fetch_object($result);
+        $id_imei = $id_imei->id_imei;
 
         //array de plantas para serem recomendadas;
         $array_plants = array();
         $list_id_plants_base = null;
 
         // plantas do perfil
-        $sql = "select * from ejbsm_recomendacao where id_imei = $id_imei->id_imei";
+        $sql = "select * from ejbsm_recomendacao where id_imei = $id_imei";
         $result = $link->query($sql) or die(mysqli_error($link));
 
 
@@ -53,10 +54,8 @@ if (isset($_GET['imei'])) {
             $i++;
         }
 
-        $list_id_plants_base."<hr>";
-
         // plantas do perfil
-        $sql = "select * from ejbsm_recomendacao where id_imei = $id_imei->id_imei ORDER BY data DESC limit 2";
+        $sql = "select * from ejbsm_recomendacao where id_imei = $id_imei ORDER BY data DESC limit 2";
         $result = $link->query($sql);
         while ($plant_base = mysqli_fetch_object($result)) {
             //seleciona os dados da planta base
@@ -69,31 +68,50 @@ if (isset($_GET['imei'])) {
             $r_p_r = $link->query($sql);
             $plant_r = mysqli_fetch_object($r_p_r);
 
+            //adiciona o id da planta atual a lista de ids base para nao ser usada novamente
+            $list_id_plants_base.=", $plant_r->id";
+
             unset($plant_r->img);
             array_push($array_plants, $plant_r);
 
         }
 
-        //$json = json_encode($array_plants);
-        //echo $json;
-
         //BUSCA DA TERCEIRA PLANTA POR PARTE DE OUTROS USUÁRIOS
-        foreach($array_plants as $p){
-            $list_not_use =$list_id_plants_base.", $p->id";
-        }
 
-        //selecionar uma planta
-
-        //selecionar todos os usuários que possuem perfil de recomendação
-        $sql = "select * from ejbsm_imei where id_imei in(select id_imei from ejbsm_recomendacao)";
+        //selecionar o id_imei da tabela de recomendações que possui uma planta igual mas nao seja o mesmo imei
+        $sql = "select id_imei from ejbsm_recomendacao where id_planta in($list_id_plants_base) and id_imei != $id_imei limit 1";
         $result = $link->query($sql);
-        while($users_r = mysqli_fetch_object($result)){
+        $imei_semelhante = mysqli_fetch_object($result);
+        $imei_semelhante = $imei_semelhante->id_imei;
 
-            //seleciona a lista de recomendação do usuário
-            $sql = "";
+        // plantas do perfil semelhante
+        $sql = "select * from ejbsm_recomendacao where id_imei = $id_imei ORDER BY data DESC limit 2";
+        $result = $link->query($sql);
+        while ($plant_base = mysqli_fetch_object($result)) {
+            //seleciona os dados da planta base
+            $sql = "select * from ejbsm_planta where id = $plant_base->id_planta";
+            $r_p_b = $link->query($sql);
+            $plant_base_ = mysqli_fetch_object($r_p_b);
+
+            //seleciona uma planta parecida que nao esta na lista
+            $sql = "select * from ejbsm_planta where id not in($list_id_plants_base) and(especie like '%$plant_base_->especie%' or ejbsm_planta.genero or '%$plant_base_->genero%' or familia like '%$plant_base_->familia%' or nome_popular like '$plant_base_->nome_popular')";
+            $r_p_r = $link->query($sql);
+            $plant_r = mysqli_fetch_object($r_p_r);
+
+            //adiciona o id da planta atual a lista de ids base para nao ser usada novamente
+            $list_id_plants_base.=", $plant_r->id";
+
+            unset($plant_r->img);
+            array_push($array_plants, $plant_r);
+
         }
 
+        /*foreach($array_plants as $p){
+            echo "<br><a target='_blank' href='webservice.php?id=$p->id&login=willian&imei=123456&lat=-29.6815435&long=-53.8074623&operation=plant'>Id: $p->id Nome: $p->nome_popular</a>";
+        }*/
 
-        //$sql = "select * from ejbsm_planta";
+        $json = json_encode($array_plants);
+        echo $json;
+
     }
 }
