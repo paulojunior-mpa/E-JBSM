35d9e9d865eab6d8f4bbcd289369b84cae03d7b7
willianrlm2 <willianw94@hotmail.com>
2015-11-03 23:36:35 -0200

    v17.5

--- webservice/search.php
+++ webservice/search.php
@@ -5,37 +5,45 @@
  * Date: 25/10/2015
  * Time: 11:07
  */
-if(isset($_GET['search'])){
-    if(!empty($_GET['search'])){
+if (isset($_GET['search'])) {
+    if (!empty($_GET['search'])) {
 
         $search = $_GET['search'];
 
         $array_filters = array('especie', 'genero', 'familia', 'nome_popular', 'descricao', 'origem');
 
-        if(isset($_GET['filter'])){
-            if(!empty($_GET['filter'])){
+        if (isset($_GET['filter'])) {
+            if (!empty($_GET['filter'])) {
                 $filter = $_GET['filter'];
                 $array_filters = array("$filter");
             }
         }
-        $sql_final = "select * from ejbsm_planta where 1 = 1 ";
+        $sql_final = "select * from ejbsm_planta where";
 
+        $j = count($array_filters);
+        $i = 1;
         foreach ($array_filters as $filter) {
-            $sql_final.= "AND $filter like '%$search%' ";
+            if($i==1)
+                $sql_final.=" ";
+            else
+                $sql_final.=" OR ";
+
+            $sql_final .= "$filter like '%$search%' ";
+            $i++;
         }
 
-        $sql_final.=" limit 5";
+        $sql_final .= " limit 5";
 
         $result = $link->query($sql_final) or die(mysqli_error($link));
 
         $array_plants = array();
 
-        if(mysqli_num_rows($result)>0) {
+        if (mysqli_num_rows($result) > 0) {
             while ($plant = mysqli_fetch_object($result)) {
                 unset($plant->img);
                 array_push($array_plants, $plant);
             }
-        }else{
+        } else {
             exit;
         }
 
