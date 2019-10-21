a1c6c4811aeef939bc80e48e5e030c86797517de
willianrlm2 <willianrlm@gmail.com>
2019-10-21 19:56:50 -0300

    Ajustes

--- app_menu.php
+++ app_menu.php
@@ -1,11 +1,11 @@
 <ul id="sidebar" class="nav nav-pills nav-stacked" style="max-width: 200px;">
     <li><a href="app.php"><span class="glyphicon glyphicon-search"></span> Plantas</a></li>
     <li><a href="app_trilha.php"><span class="glyphicon glyphicon-edit"></span> Trilhas</a></li>
-    <?phpif ($user_permissao == "administrador" or $user_permissao == "orientador") {?>
+    <?php if ($user_permissao == "administrador" or $user_permissao == "orientador") {?>
     <li><a href="app_cadastro_planta.php"><span class="glyphicon glyphicon-pencil"></span> Cadastro de plantas</a> </li>
     <li><a href="app_editar_planta.php"><span class="glyphicon glyphicon-edit"></span> Editar plantas</a></li>
     <li><a href="app_cadastro_trilha.php"><span class="glyphicon glyphicon-pencil"></span> Cadastro de trilhas</a> </li>
     <li><a href="app_editar_trilha.php"><span class="glyphicon glyphicon-edit"></span> Editar trilhas</a> </li>
     <li><a href="app_qrcode.php"><span class="glyphicon glyphicon-qrcode"></span> Gerar QR Code</a> </li>
-    <?php}?>
+    <?php } ?>
 </ul>
